0830e1409132eaf283a51e1323c75d45bb4577a6
paulsalamone <psalamone@gmail.com>
2022-02-25 13:38:21 +0100

    first pass login and register styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,72 +1,60 @@
-import {useState} from 'react';
-import axios from 'axios';
-import { Link} from 'react-router-dom';
-
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Logo from "../images/logoP-medium-color.png";
 
 const Login = () => {
   const [user, setUser] = useState({
     email: "",
-    password:""
-  })
-  
-  const [error, setError] = useState("")
+    password: "",
+  });
 
-  const handleChange = ({currentTarget: input}) => {
-    setUser({...user, [input.name]:input.value})
-  }
-  const handleSubmit = async(e) => {
-    e.preventDefault()
-    try{
-      const url = "http://localhost:5000/api/auth/login"
-      const {data: res} = await axios
-      .post(url, user)
-      localStorage.setItem("token", res.data)
-      window.location ="/"
-      console.log('logged in')
+  const [error, setError] = useState("");
 
-
-
-    } catch(error){
-      if (error.response && error.resposne.status === 400){
-        setError(error.response.messege)
+  const handleChange = ({ currentTarget: input }) => {
+    setUser({ ...user, [input.name]: input.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = "http://localhost:5000/api/auth/login";
+      const { data: res } = await axios.post(url, user);
+      localStorage.setItem("token", res.data);
+      window.location = "/";
+      console.log("logged in");
+    } catch (error) {
+      if (error.response && error.resposne.status === 400) {
+        setError(error.response.messege);
       }
     }
-  }
-console.log(user)
+  };
+  console.log(user);
   return (
-    <div>
+    <div className="form-page">
+      <img src={Logo} alt="pixasso medium logo" />
       <form onSubmit={handleSubmit}>
-        <h1>Welcome to Pixasso</h1>
-        <input 
-          type= "email"
+        <h1>Login:</h1>
+        <input
+          type="email"
           placeholder="Email"
-          name= "email"
-          value= {user.email} 
-          required 
-          onChange= {handleChange}
-        >
-        </input>
-        <input 
-          type= "password"
+          name="email"
+          value={user.email}
+          required
+          onChange={handleChange}
+        ></input>
+        <input
+          type="password"
           placeholder="Password"
-          name= "password"
-          value= {user.password} 
-          required 
-          onChange= {handleChange}
-        >
-        </input>
-        <button type="submit">
-					Submit
-				</button>
+          name="password"
+          value={user.password}
+          required
+          onChange={handleChange}
+        ></input>
+        <button type="submit">Submit</button>
         <h1>{error}</h1>
         <h1>New here?</h1>
-        <Link to="/Register">
-          New here? Please Register..
-        </Link>
-        
+        <Link to="/Register">Please Register!</Link>
       </form>
-      
-
     </div>
   );
 };
