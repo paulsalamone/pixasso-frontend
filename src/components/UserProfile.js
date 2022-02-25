b50482a4fd22e19bbb9a131cc1b49345b0f8f33e
Mimoona <memoona.raheel8@gmail.com>
2022-02-25 09:29:27 +0100

    functioning

---
+++ src/components/UserProfile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const UserProfile = () => {
+
+
+    return (
+        <>
+        <div>UserProfile</div>
+        <div>
+
+        </div>
+        </>
+  )
+}
+
+export default UserProfile
