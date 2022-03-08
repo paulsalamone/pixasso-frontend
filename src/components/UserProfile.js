d87dc4cf04325bcaa4992d3f915c0bc53157d1df
Mimoona <memoona.raheel8@gmail.com>
2022-03-08 14:19:54 +0100

    sketches displayed

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import Community from "../components/Community";
 import jwt_decode from "jwt-decode";
 import axios from "axios";
@@ -12,26 +12,78 @@ import { UserContext } from "../contexts/UserContext";
 
 
 const UserProfile = () => {
-    const [profilePicUrl, setProfilePicUrl] = useState(defaultPic);
-    // const token = localStorage.getItem("token");
-    // const decoded = jwt_decode(token);
-    // console.log(decoded);
-    const [user, setUser] = useContext(UserContext);
-    console.log(user.username)
-    const sketch= user.sketch_ids.map ((sketch) =>{
-        return(
-            sketch
-        )
-    })
-    console.log(sketch[0])
+  const [profilePicUrl, setProfilePicUrl] = useState(defaultPic);
+  const [user, setUser] = useContext(UserContext);
+  const[publishUrl, setPublishUrl] = useState([]);
+  const[unpublishUrl, setUnpublishUrl] = useState([]);
+  const[obj,setObj] =useState([])
+  
+  const[sketch, setSketch] =useState({})
+  // console.log(user)
+  // console.log(user.username)
+  // console.log(user.sketch_ids)
+  
+  useEffect(()=>{
+    getUrl()
+    console.log("hello")
+  },[])
 
-    axios
-      .get(`http://localhost:4000/api/sketch/${sketch[0]}`) 
 
-      .then(res=> console.log(res))
+  
+  const getUrl =  () => {
+    user.sketch_ids.map(async sketchId =>{
+          await axios
+          .get(`http://localhost:4000/api/sketch/${sketchId}`) 
+          .then(res=> setObj(obj=>[...obj,
+            {_id: res.data._id,
+              url:res.data.sketch_Url,
+              status:res.data.sketch_status
+          }]))
+          .catch((error) => console.log(error))   
+  })
+  }
+  console.log(obj)
 
-      .catch((error) => console.log(error));
+  const handleUnpublish = () => {
+    
 
+  }
+  
+  const handlePublish = () => {
+//     user.sketch_ids.map(async(_id) =>{
+//       await axios
+//       .get(`http://localhost:4000/api/sketch/${_id}`) 
+//       // .then(res=> console.log(res.data))
+//       .then(res =>console.log(res.data._id))
+//       .catch((error) => console.log(error));
+//       // console.log(sketch)  
+// })
+}
+  // console.log(obj)
+
+  // const section =  () => {
+  //   user.sketch_ids.map(async(sketchId) =>{
+  //         await axios
+  //         .get(`http://localhost:4000/api/sketch/${sketchId}`)
+  //         .then(res => {
+  //           if(!res.data.sketch_status){
+  //             console.log(res.data.sketch_status)
+  //             setUnpublishUrl(unpublishUrl => [...unpublishUrl, res.data.sketch_Url])
+  //             console.log(unpublishUrl)
+  //           } else {
+  //             setPublishUrl(publishUrl => [...publishUrl, res.data.sketch_Url])
+  //             console.log(publishUrl)
+  //           }
+  //         })
+  //         .catch((error) => console.log(error))   
+  // })
+  // }
+
+  // const updateStatus = () => {
+  //   axios.post()    i need  sketch id here and cprrespong to that id i can update sketch status using put route
+
+
+  // }
 
 
   const placeholderBio =
@@ -61,60 +113,52 @@ const UserProfile = () => {
         <div className="gallery-section">
           <h2>Unpublished Sketches</h2>
           <div className="gallery-grid">
-            <div className="gallery-cell">
-              <img src={GalleryPlaceholder1} />
-              <button onClick={<Community />}>publish</button>
-            </div>
-            <div className="gallery-cell">
-              <img src={GalleryPlaceholder2} />
-              <button onClick={<Community />}>publish</button>
-            </div>
-            <div className="gallery-cell">
-              <img src={GalleryPlaceholder3} />
-              <button onClick={<Community />}>publish</button>
-            </div>
-            <div className="gallery-cell">
-              <img src={GalleryPlaceholder4} />
-              <button onClick={<Community />}>publish</button>
-            </div>
-            <div className="gallery-cell">
-              <img src={GalleryPlaceholder1} />
-              <button onClick={<Community />}>publish</button>
-            </div>
+          {/* {obj.map(each =>{
+            return(
+              <>
+              {console.log(each._id)}
+              </>
+            )
+
+          })}   */}
+
+               
+              {obj.filter(e => e.status === false)
+              .map(e => {
+                return(
+                  <div className="gallery-cell">
+                    <img src={e.url} />
+                    <button onClick={handlePublish}>Publish</button>
+                  </div>
+
+                )
+              })}
+
+            
           </div>
         </div>
+          
         <div className="gallery-section">
           <h2>Published Sketches</h2>
           <div className="gallery-grid">
-            <div className="gallery-cell">
-              <img src={GalleryPlaceholder2} />
-              <button onClick={<Community />}>unpublish</button>
+          {obj.filter(e => e.status === true)
+              .map(e => {
+                return(
+                  <div className="gallery-cell">
+                    <img src={e.url} />
+                    <button onClick={handleUnpublish}>Unpublish</button>
+                  </div>
+
+                )
+              })}
+            
             </div>
-            <div className="gallery-cell">
-              <img src={GalleryPlaceholder3} />
-              <button onClick={<Community />}>unpublish</button>
             </div>
-            <div className="gallery-cell">
-              <img src={GalleryPlaceholder4} />
-              <button onClick={<Community />}>unpublish</button>
-            </div>
-            <div className="gallery-cell">
-              <img src={GalleryPlaceholder1} />
-              <button onClick={<Community />}>unpublish</button>
-            </div>
-            <div className="gallery-cell">
-              <img src={GalleryPlaceholder2} />
-              <button onClick={<Community />}>unpublish</button>
-            </div>
-          </div>
-        </div>
       </div>
-    </div>
+
+     </div>
   );
 };
 
-{
-  /* <img src="https://res.cloudinary.com/pixasso/image/upload/v1646300481/ae6cct2tky57obshjhgx.jpg" /> */
-}
 
 export default UserProfile;
