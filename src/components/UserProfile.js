f2268ffe557606774db33f6a6231be5768faa026
paulsalamone <psalamone@gmail.com>
2022-03-08 20:53:01 +0100

    attempt to solve function problem

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -4,45 +4,42 @@ import jwt_decode from "jwt-decode";
 import axios from "axios";
 
 import defaultPic from "../images/profilepic.jpg";
-import GalleryPlaceholder1 from "../images/gallery-placeholder1.png";
-import GalleryPlaceholder2 from "../images/gallery-placeholder2.png";
-import GalleryPlaceholder3 from "../images/gallery-placeholder3.png";
-import GalleryPlaceholder4 from "../images/gallery-placeholder4.png";
-import { UserContext } from "../contexts/UserContext";
 
+import { UserContext } from "../contexts/UserContext";
 
 const UserProfile = () => {
   const [profilePicUrl, setProfilePicUrl] = useState(defaultPic);
   const [user, setUser] = useContext(UserContext);
-  const[obj,setObj] =useState([])
+  const [obj, setObj] = useState([]);
 
-  // console.log(user)
+  // console.log(user);
   // console.log(user.username)
-  console.log(user.sketch_ids)
+  // console.log(user.sketch_ids);
 
-  const handleUnpublish = async(id) => {
-      await axios
-      .put(`http://localhost:4000/api/sketch/${id}`, {
-        sketch_status: false
+  // let test = `http://localhost:4000/api/sketch/62276eef5ee22a2fb5e66357`;
+  // console.log(test);
+
+  const handlePublish = async (e) => {
+    e.preventDefault();
+    await axios
+      .put(`http://localhost:4000/api/sketch/${e.target.sketchid.value}`, {
+        sketch_status: true,
       })
-      .then(res => console.log(res))
-      .catch((error) => console.log(error))
-      console.log("unpublish");
-    
-  }
-  
-  const handlePublish = (id) => {
-    console.log("xxxxx")
-        // await axios
-        // .put(`http://localhost:4000/api/sketch/${id}`, {
-        //   sketch_status: true
-        // })
-        // .then(res => console.log(res))
-        // .then(console.log("publish"))
-        // .catch((error) => console.log(error))
-}
- 
+      .then((res) => console.log(res))
+      .then(console.log("publish"))
+      .catch((error) => console.log(error));
+  };
 
+  const handleUnpublish = async (e) => {
+    e.preventDefault();
+    await axios
+      .put(`http://localhost:4000/api/sketch/${e.target.sketchid.value}`, {
+        sketch_status: false,
+      })
+      .then((res) => console.log(res))
+      .then(console.log("publish"))
+      .catch((error) => console.log(error));
+  };
 
   const placeholderBio =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
@@ -71,47 +68,59 @@ const UserProfile = () => {
         <div className="gallery-section">
           <h2>Unpublished Sketches</h2>
           <div className="gallery-grid">
-
-          {user.sketch_ids && user.sketch_ids
-          .filter(sketch =>sketch.sketch_status=== false)
-          .map(element => {
-              return (
-                <>
-                <div className="gallery-cell">
-                    <img src={element.sketch_url} />
-                    <button onClick={handlePublish}>Publish</button>
-                  </div>                  
-                </>
-              )
-          })}
+            {user.sketch_ids &&
+              user.sketch_ids
+                .filter((sketch) => sketch.sketch_status === false)
+                .map((element) => {
+                  return (
+                    <>
+                      <div className="gallery-cell">
+                        <form onSubmit={handlePublish}>
+                          <img src={element.sketch_url} />
+                          <input
+                            name="sketchid"
+                            type="text"
+                            value={element._id}
+                            style={{ display: "none" }}
+                          ></input>
+                          <button type="submit">Publish</button>
+                        </form>
+                      </div>
+                    </>
+                  );
+                })}
           </div>
         </div>
 
-          
         <div className="gallery-section">
           <h2>Published Sketches</h2>
           <div className="gallery-grid">
-          {user.sketch_ids && user.sketch_ids
-          .filter(sketch =>sketch.sketch_status=== true)
-          .map(element => {
-              return (
-                <>
-                <div className="gallery-cell">
-                    <img src={element.sketch_url} />
-                    <button onClick={handleUnpublish}>Unpublish</button>
-                </div>
-                  
-                </>
-              )
-          })}
-            
-            </div>
-            </div>
+            {user.sketch_ids &&
+              user.sketch_ids
+                .filter((sketch) => sketch.sketch_status === true)
+                .map((element) => {
+                  return (
+                    <>
+                      <div className="gallery-cell">
+                        <form onSubmit={handleUnpublish}>
+                          <img src={element.sketch_url} />
+                          <input
+                            name="sketchid"
+                            type="text"
+                            value={element._id}
+                            style={{ display: "none" }}
+                          ></input>
+                          <button type="submit">Unpublish</button>
+                        </form>
+                      </div>
+                    </>
+                  );
+                })}
+          </div>
+        </div>
       </div>
-
-     </div>
+    </div>
   );
 };
-
 
 export default UserProfile;
