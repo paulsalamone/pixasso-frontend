7dff59a2299e39e27e32fee482edc983e912e8c7
Mimoona <memoona.raheel8@gmail.com>
2022-03-09 16:02:23 +0100

    pagination

--- src/components/algorithms/Algo1.js
+++ src/components/algorithms/Algo1.js
@@ -200,12 +200,17 @@ const Algo1 = (props) => {
             await axios
               .post("http://localhost:4000/api/sketch/upload", {
                 sketch_url: url,
+                // posted_by:user.id
               })
-              .then((res) => 
+              .then((res) =>
                 axios.put(`http://localhost:4000/api/users/${user.id}`, {
                   sketch_ids : res.data._id
                 })
+                .put(`http://localhost:4000/api/sketch/${res.data._id}`,{
+                  posted_by: user.id
+                })
               )
+              
               .then(res => console.log(res));
           })
           .catch((err) => console.log(err));
